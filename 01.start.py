4b41de997d4772307d353f8175bd38d6dcdfea98
jongsung-qa <jongsung.jace@gmail.com>
2020-09-02 22:46:38 +0900

    Update 01.start.py

--- 01.start.py
+++ 01.start.py
@@ -1,10 +1,15 @@
-# 01.sum 1 ~ 100
-
+# sum 1 ~ 10
 def sum(n):
     s = 0
     for i in range(1, n+1):
         s = s + i
     return s
 print(sum(10))
+
+# sum 1 ~ 100
+def sum1(n):
+    return n*(n+1)//2
+print(sum1(100))
+
 
 
