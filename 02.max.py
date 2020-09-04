49894247593c74a47c9ca39e8006de01d68167b8
jongsung-qa <jongsung.jace@gmail.com>
2020-09-04 21:53:52 +0900

    Update 02.max.py

--- 02.max.py
+++ 02.max.py
@@ -1,1 +1,13 @@
 # find max
+
+a = [1,5,3,30,21,7]
+
+def max(a):
+    n = len(a)
+    max_v = a[0]
+    for i in range(1,n):
+        if a[i] > max_v:
+            max_v = a[i]
+    return max_v
+
+print(max(a))
