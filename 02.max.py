b981981fb56055dbdab1b272a3c3ecf47fbe76d3
jongsung-qa <jongsung.jace@gmail.com>
2020-09-04 21:55:21 +0900

    Update 02.max.py

--- 02.max.py
+++ 02.max.py
@@ -4,10 +4,9 @@ a = [1,5,3,30,21,7]
 
 def max(a):
     n = len(a)
-    max_v = a[0]
+    max_v = a[0] # 1st value => max_v
     for i in range(1,n):
-        if a[i] > max_v:
+        if a[i] > max_v: # compare value
             max_v = a[i]
     return max_v
-
 print(max(a))
